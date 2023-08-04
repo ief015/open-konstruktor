d67d6ccc87326b7fccfe32dcc118c0b66fd6b655
Nate <ief015@gmail.com>
2023-08-04 19:36:40 -0400

    GateNode.type -> GateNode.isNPN
    smaller memory footprint

--- src/simulation/GateNode.ts
+++ src/simulation/GateNode.ts
@@ -2,18 +2,13 @@ import PathNode from "./PathNode";
 
 export type GateType = 'pnp' | 'npn';
 
-/**
- * 0: NPN, 1: PNP
- */
-export type GateTypeEnum = number;
-
 export default class GateNode {
   public gatedPaths: PathNode[] = []; // Paths of controlled flow
   public switchingPaths: PathNode[] = []; // Paths of switching state
   public active: boolean = false;
-  public type: GateTypeEnum = 0;
+  public isNPN: boolean = true;
 
   constructor(type: GateType = 'npn') {
-    this.type = type === 'npn' ? 0 : 1;
+    this.isNPN = type === 'npn';
   }
 }
