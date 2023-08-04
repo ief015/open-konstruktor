8c70983351d0c816f825b44a901fda6a1094c88b
Nate <ief015@gmail.com>
2023-08-04 19:08:12 -0400

    labels on pin nodes

--- src/simulation/PinNode.ts
+++ src/simulation/PinNode.ts
@@ -1,10 +1,19 @@
 import PathNode from "./PathNode";
 
 export default class PinNode extends PathNode {
-  public active: boolean = false;
+  public active: boolean;
+  public label: string;
 
-  constructor(active: boolean = false) {
+  constructor(label?: string)
+  constructor(active: boolean)
+  constructor(label: string, active: boolean)
+  constructor(label: string|boolean = '', active: boolean = false) {
     super();
+    if (typeof label === 'boolean') {
+      active = label;
+      label = '';
+    }
+    this.label = label;
     this.active = active;
   }
 }
