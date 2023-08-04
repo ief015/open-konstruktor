0034d8ce13e2956730fc0477b62cf59e4b9d66c3
Nate <ief015@gmail.com>
2023-08-04 00:30:27 -0400

    sr latch simulation on start script

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { PathNode, GateNode, PinNode, Network, Timeline } from "./simulation";
+
+const pinVCC = new PinNode(true);
+const pinS = new PinNode();
+const pinR = new PinNode();
+const pinQ = new PinNode();
+
+const pathVCC = new PathNode();
+const pathS = new PathNode();
+const pathR = new PathNode();
+const pathQ = new PathNode();
+
+const pnp = new GateNode('pnp');
+const npn = new GateNode('npn');
+
+const network = new Network(
+  [pathVCC, pathS, pathR, pathQ],
+  [pnp, npn],
+  [pinVCC, pinS, pinR, pinQ],
+);
+
+pinVCC.connectedPaths.push(pathVCC);
+pinS.connectedPaths.push(pathS);
+pinR.connectedPaths.push(pathR);
+pinQ.connectedPaths.push(pathQ);
+
+npn.gatedPaths.push(pathVCC);
+npn.gatedPaths.push(pathQ);
+npn.switchingPaths.push(pathS);
+
+pnp.gatedPaths.push(pathS);
+pnp.gatedPaths.push(pathQ);
+pnp.switchingPaths.push(pathR);
+
+const tl = new Timeline();
+
+tl.addKeyframe(2, pinS, true);
+tl.addKeyframe(4, pinS, false);
+
+tl.addKeyframe(6, pinR, true);
+tl.addKeyframe(8, pinR, false);
+
+tl.addKeyframe(10, pinVCC, false);
+
+console.log('VCC\tS\tR\tQ');
+tl.play(network, (frame) => {
+  console.log(`${Number(pinVCC.state)}\t${Number(pinS.state)}\t${Number(pinR.state)}\t${Number(pinQ.state)}`);
+});
