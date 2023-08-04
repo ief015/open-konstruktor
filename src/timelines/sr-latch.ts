8287c4c5ca5c20d2ed2bec03ad120cb046baf2c6
Nate <ief015@gmail.com>
2023-08-04 03:20:17 -0400

    middle dot works well enough

--- src/timelines/sr-latch.ts
+++ src/timelines/sr-latch.ts
@@ -1,6 +1,6 @@
 import { PathNode, GateNode, PinNode, Network, Timeline } from "../simulation";
 
-const stateStr = (state: boolean) => state ? '1' : '-';
+const stateStr = (state: boolean) => state ? '1' : '·';
 
 export default function play() {
 
