7a67300aa9a1c2428e88e45b3f2a840f78d88ddb
Nate <ief015@gmail.com>
2023-08-04 20:21:34 -0400

    assertPin state

--- src/unit-test/index.ts
+++ src/unit-test/index.ts
@@ -1,3 +1,4 @@
+import { PinNode } from "../simulation";
 import tests from "./tests";
 
 export type UnitTest = () => Promise<void>;
@@ -5,6 +6,11 @@ export type UnitTest = () => Promise<void>;
 export function assertEqual<T>(actual: T, expected: T) {
   if (actual !== expected) {
     throw new Error(`Expected ${expected}, got ${actual}`);
+  }
+}
+export function assertPin(pin: PinNode, expected: boolean|number) {
+  if (pin.state != expected) {
+    throw new Error(`Pin ${pin.label}: Expected ${!!expected}, got ${pin.state}`);
   }
 }
 
