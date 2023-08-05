e7817bfac00e0441507a7cc509e22093fb14d91d
Nate <ief015@gmail.com>
2023-08-05 19:41:41 -0400

    decode save strings to bin

---
+++ src/serialization/decode.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+export type Base64String = string;
+
+export default async function decode(saveData: Base64String, outFile?: string): Promise<Buffer> {
+  const saveBuffer = Buffer.from(saveData.replace(/\s/g, ''), 'base64');
+  const inflated = await new Promise<Buffer>((resove, reject) => {
+    zlib.inflate(saveBuffer, (err, res) => (err ? reject(err) : resove(res)));
+  });
+  if (outFile) {
+    await new Promise<void>((resolve, reject) => {
+      fs.writeFile(outFile, inflated, (err) => (err ? reject(err) : resolve()));
+    });
+  }
+  return inflated;
+}
