eb9835a575da2632f4e02d5dcfb1297fa1adcd77
Nate <ief015@gmail.com>
2023-08-04 22:16:28 -0400

    4-input and/or gate unit test

--- src/unit-test/tests.ts
+++ src/unit-test/tests.ts
@@ -2,14 +2,16 @@ import { UnitTest } from ".";
 
 import npn from "./tests/npn";
 import pnp from "./tests/pnp";
-import srlatch from "./tests/sr-latch";
-import linedecoder from "./tests/2to4-line-decoder";
+import srLatch from "./tests/sr-latch";
+import lineDecoder from "./tests/2to4-line-decoder";
+import andOrGate from "./tests/4-input-and-or-gate";
 
 const tests: Record<string, UnitTest> = {
   npn,
   pnp,
-  srlatch,
-  linedecoder,
+  srLatch,
+  lineDecoder,
+  andOrGate,
 };
 
 export default tests;
---
+++ src/unit-test/tests/4-input-and-or-gate.ts
@@ -0,0 +1,180 @@
+import { assertPin } from "..";
+import { PathNode, GateNode, PinNode, Network, Timeline } from "../../simulation";
+
+// Based on KT141AO - 4-INPUT AND/OR GATE design:
+/*
+eNrtmkEOgjAQRWU+G87gFdx7Fu9/EZMKgkCnhQoRfDRdmNcfkJCXKUN9q6/No2ru
+VX1ZMwguDloYi4PVeywK2sfYIzi8VFt7V/cP8qwSJEiQIMFjB0M5Z/EjUEmzs5wG
+rvDbutmf1wYLErRdMU+n/6inFqPmZhP0tcKn8qhBodAY3dJIMRNmqRIaoeJuQKH/
+Qc+uynHltyCrgiwUCkWVB5KhJqXfcAOePq9ckYqKFApFlaevG+Ury3+TmaE7ZAiF
+ospjUHcTLbcyVHoDLh4tKBRV9vHCHrd1Xax4F9tcqrGacoWW0eO2gh63XzcqoVn6
+mFDoOrpNj/srqpyf5bT1pPzLTihr/EnQj9R+4l0lFLoJ3dJIkeMJ5zrHaQ==
+*/
+
+export default async function() {
+
+  const pinVCC = new PinNode('VCC', true);
+  const pinA = new PinNode('A');
+  const pinB = new PinNode('B');
+  const pinC = new PinNode('C');
+  const pinD = new PinNode('D');
+  const pinX = new PinNode('X');
+  const pinY = new PinNode('Y');
+
+  const gp1 = new GateNode('pnp');
+  const gp2 = new GateNode('pnp');
+  const gp3 = new GateNode('pnp');
+  const gp4 = new GateNode('pnp');
+  const gp5 = new GateNode('pnp');
+
+  const pp1 = new PathNode();
+  const pp2 = new PathNode();
+  const pp3 = new PathNode();
+  const pp4 = new PathNode();
+
+  const gn1 = new GateNode('npn');
+  const gn2 = new GateNode('npn');
+  const gn3 = new GateNode('npn');
+
+  const pn1 = new PathNode();
+  const pn2 = new PathNode();
+
+  gn1.gatedPaths.push(pinA, pn1);
+  gn1.switchingPaths.push(pinB);
+
+  gn2.gatedPaths.push(pn1, pn2);
+  gn2.switchingPaths.push(pinC);
+
+  gn3.gatedPaths.push(pn2, pinX);
+  gn3.switchingPaths.push(pinD);
+
+  gp1.gatedPaths.push(pinVCC, pp1);
+  gp1.switchingPaths.push(pinA);
+  
+  gp2.gatedPaths.push(pp1, pp2);
+  gp2.switchingPaths.push(pinB);
+  
+  gp3.gatedPaths.push(pp2, pp3);
+  gp3.switchingPaths.push(pinC);
+  
+  gp4.gatedPaths.push(pp3, pp4);
+  gp4.switchingPaths.push(pinD);
+
+  gp5.gatedPaths.push(pinVCC, pinY);
+  gp5.switchingPaths.push(pp4);
+
+  const network = new Network([
+    pinVCC, pinA, pinB, pinC, pinD, pinX, pinY,
+    pp1, pp2, pp3, pp4, pn1, pn2,
+    gp1, gp2, gp3, gp4, gp5, gn1, gn2, gn3
+  ]);
+
+  const tl = new Timeline(network);
+
+  // Pin A
+  for (let i = 0; i < 10; i++) {
+    tl.addKeyFrame(i * 20 + 10, pinA, true);
+    tl.addKeyFrame(i * 20 + 20, pinA, false);
+  }
+  tl.addKeyFrame(210, pinA, true);
+  tl.addKeyFrame(240, pinA, false);
+  tl.addKeyFrame(250, pinA, true);
+  tl.addKeyFrame(270, pinA, false);
+
+  // Pin B
+  tl.addKeyFrame(20, pinB, true);
+  tl.addKeyFrame(40, pinB, false);
+  tl.addKeyFrame(60, pinB, true);
+  tl.addKeyFrame(80, pinB, false);
+  tl.addKeyFrame(90, pinB, true);
+  tl.addKeyFrame(120, pinB, false);
+  tl.addKeyFrame(140, pinB, true);
+  tl.addKeyFrame(160, pinB, false);
+  tl.addKeyFrame(190, pinB, true);
+  tl.addKeyFrame(200, pinB, false);
+  tl.addKeyFrame(220, pinB, true);
+  tl.addKeyFrame(240, pinB, false);
+  tl.addKeyFrame(260, pinB, true);
+  tl.addKeyFrame(270, pinB, false);
+
+  // Pin C
+  tl.addKeyFrame(30, pinC, true);
+  tl.addKeyFrame(80, pinC, false);
+  tl.addKeyFrame(90, pinC, true);
+  tl.addKeyFrame(100, pinC, false);
+  tl.addKeyFrame(120, pinC, true);
+  tl.addKeyFrame(160, pinC, false);
+  tl.addKeyFrame(200, pinC, true);
+  tl.addKeyFrame(240, pinC, false);
+  tl.addKeyFrame(260, pinC, true);
+  tl.addKeyFrame(270, pinC, false);
+
+  // Pin D
+  tl.addKeyFrame(30, pinD, true);
+  tl.addKeyFrame(40, pinD, false);
+  tl.addKeyFrame(80, pinD, true);
+  tl.addKeyFrame(160, pinD, false);
+  tl.addKeyFrame(200, pinD, true);
+  tl.addKeyFrame(210, pinD, false);
+  tl.addKeyFrame(220, pinD, true);
+  tl.addKeyFrame(240, pinD, false);
+  tl.addKeyFrame(250, pinD, true);
+  tl.addKeyFrame(270, pinD, false);
+
+  tl.run(280, frame => {
+    // Probe various frames for expected values
+    switch (frame) {
+      case 0:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 1:
+      case 10:
+      case 11:
+        assertPin(pinX, false);
+        assertPin(pinY, false);
+        break;
+        break;
+      case 12:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 30:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 31:
+        assertPin(pinX, true);
+        assertPin(pinY, true);
+        break;
+      case 30:
+        assertPin(pinX, false);
+        assertPin(pinY, false);
+        break;
+      case 90:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 91:
+        assertPin(pinX, true);
+        assertPin(pinY, true);
+        break;
+      case 100:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 160:
+      case 161:
+        assertPin(pinX, false);
+        assertPin(pinY, true);
+        break;
+      case 162:
+        assertPin(pinX, false);
+        assertPin(pinY, false);
+        break;
+    }
+  });
+
+  // tl.printHistory();
+
+}
