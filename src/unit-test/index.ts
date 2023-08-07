f7c1b2f499426e64e5053653ae46f7d94f9b0c9a
Nate <ief015@gmail.com>
2023-08-07 00:28:32 -0400

    assertEqualArray

--- src/unit-test/index.ts
+++ src/unit-test/index.ts
@@ -8,6 +8,17 @@ export function assertEqual<T>(actual: T, expected: T) {
   }
 }
 
+export function assertEqualArray<T>(actual: T[], expected: T[]) {
+  if (actual.length !== expected.length) {
+    throw new Error(`Expected array length ${expected.length}, got ${actual.length}`);
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      throw new Error(`Expected ${expected[i]} at index ${i}, got ${actual[i]}`);
+    }
+  }
+}
+
 export function assertPin(pin: PinNode, expected: boolean|number) {
   if (pin.state != expected) {
     throw new Error(`Pin ${pin.label}: Expected ${!!expected}, got ${pin.state}`);
