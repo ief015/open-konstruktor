0fff086206fe30b159007661e1546f5b8fd32bda
Nate <ief015@gmail.com>
2023-08-19 21:58:42 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [ '@nuxtjs/tailwindcss' ],
   alias: {
     '@/*': './*',
