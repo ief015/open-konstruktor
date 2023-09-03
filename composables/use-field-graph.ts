437a93dcc5e183b057be573b61319174f3d0fb38
Nate <ief015@gmail.com>
2023-09-03 01:30:21 -0400

    use-field-graph dimensions

--- composables/use-field-graph.ts
+++ composables/use-field-graph.ts
@@ -3,20 +3,30 @@ import { FieldGraph } from "@/simulation";
 
 const field = ref<FieldGraph>();
 const designScore = ref(0);
+const dimensions = reactive<LayerDimensions>({ columns: 0, rows: 0 });
 
 const updateDesignScore = () => {
   designScore.value = field.value?.getDesignScore() ?? 0;
+}
+
+const updateDimensions = () => {
+  if (!field.value) throw new Error("FieldGraph is not initialized");
+  const { columns, rows } = field.value.getDimensions();
+  dimensions.columns = columns;
+  dimensions.rows = rows;
 }
 
 const loadBlank = (columns?: number, rows?: number, pinRows?: number) => {
   const data = new DesignData(columns, rows, pinRows);
   field.value = new FieldGraph(data);
   updateDesignScore();
+  updateDimensions();
 }
 
 const load = (saveString: string) => {
   field.value = FieldGraph.from(saveString);
   updateDesignScore();
+  updateDimensions();
 }
 
 loadBlank();
@@ -25,6 +35,7 @@ export default function useFieldGraph() {
   return {
     field,
     designScore: readonly(designScore),
+    dimensions: readonly(dimensions),
     loadBlank,
     load,
     updateDesignScore,
