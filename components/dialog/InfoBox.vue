1d7c7991a45c530193b67405e48eb9e2e1b536bd
Nate <ief015@gmail.com>
2023-09-14 02:33:42 -0400

    infobox body too hard to read. use mono

--- components/dialog/InfoBox.vue
+++ components/dialog/InfoBox.vue
@@ -9,7 +9,7 @@
           🗙
         </div>
       </div>
-      <div class="font-ttw text-sm h-[200px] overflow-auto">
+      <div class="font-mono text-sm h-[200px] overflow-auto">
         <div v-html="contentHtml" />
       </div>
       <div class="flex flex-row justify-center gap-4">
