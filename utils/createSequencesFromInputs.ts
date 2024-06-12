8fc8c9efa308f30d4b018cbc2e04333da14e63a9
Nate <ief015@gmail.com>
2024-06-12 00:14:00 -0400

    added delta context to sequencer, reducer -> mapper

--- utils/createSequencesFromInputs.ts
+++ utils/createSequencesFromInputs.ts
@@ -1,27 +1,49 @@
 import { Sequence } from "@/simulation";
 
+interface SequencerContext<TState extends object> {
+  /**
+   * The current state of the sequencer.
+   */
+  state: TState;
+  /**
+   * Input values for each input sequence in the current frame.
+   */
+  inputs: boolean[];
+  /**
+   * The changes of each input since the last frame.
+   * 1: input was set to true
+   * 0: input was not changed
+   * -1: input was set to false
+   */
+  deltas: number[];
+  /**
+   * The current frame number.
+   */
+  frame: number;
+}
+
 function createSequencesFromInputs<TState extends object>(
   inputSequences: Sequence[],
-  reducer: (context: { state: TState, inputs: boolean[], frame: number }) => boolean[],
+  mapper: (context: SequencerContext<TState>) => boolean[],
   initialState?: TState,
 ): Sequence[];
 
 function createSequencesFromInputs<TState extends object>(
   inputSequences: Sequence[],
-  reducer: (context: { state: TState, inputs: boolean[], frame: number }) => boolean[],
+  mapper: (context: SequencerContext<TState>) => boolean[],
   maxLength: number,
 ): Sequence[];
 
 function createSequencesFromInputs<TState extends object>(
   inputSequences: Sequence[],
-  reducer: (context: { state: TState, inputs: boolean[], frame: number }) => boolean[],
+  mapper: (context: SequencerContext<TState>) => boolean[],
   initialState: TState,
   maxLength: number,
 ): Sequence[];
 
 function createSequencesFromInputs<TState extends object>(
   inputSequences: Sequence[],
-  reducer: (context: { state: TState, inputs: boolean[], frame: number }) => boolean[],
+  mapper: (context: SequencerContext<TState>) => boolean[],
   initialState?: number|TState,
   maxLength?: number,
 ): Sequence[] {
@@ -32,17 +54,20 @@ function createSequencesFromInputs<TState extends object>(
   const state = <TState>{ ...initialState };
   maxLength = maxLength || inputSequences.reduce((max, seq) => Math.max(max, seq.getLength()), 0);
   const inputs = inputSequences.map(seq => seq.getFront());
+  const deltas: number[] = [];
   const outputSequences: Sequence[] = [];
   for (let frame = 0; frame < maxLength; frame++) {
     for (let i = 0; i < inputSequences.length; i++) {
       const seq = inputSequences[i];
       const newState = seq.getFrames()[frame];
+      const lastState = inputs[i];
+      deltas[i] = (newState !== lastState) ? (newState ? 1 : -1) : 0;
       if (newState === undefined) {
         continue;
       }
       inputs[i] = newState;
     }
-    const outputs = reducer({ state, inputs, frame });
+    const outputs = mapper({ state, inputs, deltas, frame });
     for (let i = 0; i < outputs.length; i++) {
       const seq = outputSequences[i] ?? new Sequence();
       if (seq.getBack() !== outputs[i]) {
