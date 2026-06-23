aae99b54e43bb361541988e907ddd7808fd6b169
Nate <ief015@gmail.com>
2026-06-23 16:10:20 -0400

    SequenceRecorder: keep track of recording length, allow custom recordable funcs (for probes, etc)

--- simulation/SequenceRecorder.ts
+++ simulation/SequenceRecorder.ts
@@ -1,12 +1,15 @@
 import { getNodeState, Sequence, type NetworkNode } from '@/simulation';
 
-type RecordingMap = Map<NetworkNode, Sequence>;
+export type RecordableFn = (frame: number) => boolean | null;
+export type Recordable = NetworkNode | RecordableFn;
+type RecordingMap = Map<Recordable, Sequence>;
 
 /**
  * Handles recording node states to sequences.
  */
 export class SequenceRecorder {
   protected recordings: RecordingMap = new Map();
+  protected recordingLength?: number = 0;
 
   public [Symbol.iterator](): Iterator<Sequence> {
     return this.recordings.values();
@@ -15,22 +18,36 @@ export class SequenceRecorder {
   /**
    * Get the recording for a node.
    */
-  public getRecording(node: NetworkNode): Sequence | undefined {
+  public get(node: Recordable): Sequence | undefined {
     return this.recordings.get(node);
   }
 
   /**
    * Get all recordings.
    */
-  public getRecordings() {
+  public entries() {
     return this.recordings.entries();
+  }
+
+  /**
+   * Get all recording sequences.
+   */
+  public values() {
+    return this.recordings.values();
+  }
+
+  /**
+   * Get the length of the recording.
+   */
+  public getRecordingLength(): number {
+    return this.recordingLength ?? 0;
   }
 
   /**
    * Add a node to the recorder.
    * @param node
    */
-  public add(node: NetworkNode): void {
+  public add(node: Recordable) {
     if (!this.recordings.has(node)) {
       this.recordings.set(node, new Sequence());
     }
@@ -40,37 +57,55 @@ export class SequenceRecorder {
    * Remove a node from the recorder.
    * @param node
    */
-  public remove(node: NetworkNode): void {
-    this.recordings.delete(node);
+  public remove(node: Recordable) {
+    return this.recordings.delete(node);
   }
 
   /**
    * Remove all nodes from the recorder.
    */
-  public clear(): void {
+  public clear() {
     this.recordings.clear();
+    this.recordingLength = 0;
   }
 
   /**
    * Clear recordings.
    */
-  public reset(): void {
+  public reset() {
     for (const [node, sequence] of this.recordings) {
       sequence.clear();
     }
+    this.recordingLength = 0;
+  }
+
+  protected getRecordableValue(
+    node: Recordable,
+    frame: number,
+  ): boolean | null {
+    if (typeof node === 'function') {
+      return node(frame);
+    }
+    return getNodeState(node);
   }
 
   /**
    * Record nodes to sequences.
    * @param frame Record to a specific frame.
    */
-  public record(frame?: number): void {
+  public record(frame?: number) {
     for (const [node, sequence] of this.recordings) {
-      const at = frame ?? sequence.getLength();
-      const state = getNodeState(node);
-      if (sequence.probe(at) !== state) {
+      const len = sequence.getLength();
+      const at = frame ?? len;
+      const state = this.getRecordableValue(node, at);
+      if (state === null) continue;
+      if (at < len || sequence.getBack() !== state) {
         sequence.setFrame(at, state);
       }
     }
+    this.recordingLength = Math.max(
+      this.recordingLength ?? 0,
+      (frame ?? 0) + 1,
+    );
   }
 }
