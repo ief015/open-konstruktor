ce7e12b03ecabdaa868d0d84657bc7aa4f06e6f2
Nate <ief015@gmail.com>
2026-06-25 20:32:06 -0400

    open/close new simulations

--- app.vue
+++ app.vue
@@ -10,33 +10,62 @@
     </template>
     <template #left>
       <div class="w-[10em] h-full right-border flex flex-col">
-        <div class="text-sm font-ttw text-center m-1 select-none">Designs</div>
-        <ToolboxDesignSelect class="flex-1" />
-        <div class="text-sm font-ttw text-center m-1 select-none">Snippets</div>
-        <ToolboxSnippetsSelect class="flex-1" />
+        <template v-if="currentSimulation">
+          <div class="text-sm font-ttw text-center m-1 select-none">
+            Designs
+          </div>
+          <ToolboxDesignSelect class="flex-1" />
+          <div class="text-sm font-ttw text-center m-1 select-none">
+            Snippets
+          </div>
+          <ToolboxSnippetsSelect class="flex-1" />
+        </template>
       </div>
     </template>
-    <div class="absolute inset-0">
-      <div class="flex flex-col h-full">
-        <div class="flex-1 overflow-hidden">
-          <CircuitField />
+    <div class="flex flex-col h-full">
+      <TabBar
+        class="bottom-border"
+        v-model="currentTab"
+        :items="tabItems"
+        @selected="onSelectedTab"
+        @close="onCloseTab"
+      >
+        <template #before="{ name }">
+          <CircuitStatusLight
+            class="px-1"
+            :is-running="unref(getSimulationByName(name)?.isRunning)"
+            :is-paused="unref(getSimulationByName(name)?.isPaused)"
+          />
+        </template>
+      </TabBar>
+      <div class="flex-1 relative bg-neutral-900">
+        <div class="flex flex-col h-full" v-if="currentSimulation">
+          <div class="flex-1 overflow-hidden">
+            <CircuitField />
+          </div>
+          <div class="top-border">
+            <CircuitScope />
+          </div>
+          <div class="top-border py-2 bg-neutral-800">
+            <CircuitControls class="w-full h-full" />
+          </div>
         </div>
-        <div class="top-border">
-          <CircuitScope />
+        <div
+          class="absolute inset-x-0 top-[1.5em] flex flex-row justify-center"
+        >
+          <DialogInfoBox class="m-2 w-[1000px]" v-if="currentSimulation" />
+          <DialogWelcome />
         </div>
-        <div class="top-border py-2">
-          <CircuitControls class="w-full h-full" />
-        </div>
-      </div>
-      <div class="absolute inset-x-0 top-0 flex flex-row justify-center">
-        <DialogInfoBox class="m-2 w-[1000px]" />
-        <DialogWelcome />
       </div>
     </div>
     <template #right>
       <div class="w-[5em] h-full left-border flex flex-col">
-        <div class="text-sm font-ttw text-center m-1 select-none">Toolbox</div>
-        <ToolboxControls class="mx-2" />
+        <template v-if="currentSimulation">
+          <div class="text-sm font-ttw text-center m-1 select-none">
+            Toolbox
+          </div>
+          <ToolboxControls class="mx-2" />
+        </template>
       </div>
     </template>
     <template #bottom>
@@ -60,6 +89,11 @@
 </template>
 
 <script setup lang="ts">
+import { useWelcomeDialogListener } from '@/components/dialog/welcome/welcome-events';
+import { useMenuBarListener } from '@/components/menu/bar-app-events';
+import type { TabBarItem } from '@/components/tab/bar-item.vue';
+import type { ShallowRef } from 'vue';
+
 const config = useRuntimeConfig();
 const status = useStatusBar();
 
@@ -67,8 +101,87 @@ const status = useStatusBar();
 const clipboard = useClipboard();
 provide('clipboard', clipboard);
 
-const currentSimulation = shallowRef(useCircuitSimulation());
-provideCircuitSimulation(currentSimulation);
+const simulations: ReturnType<typeof useCircuitSimulation>[] = shallowReactive(
+  [],
+);
+const currentSimulation = shallowRef<ReturnType<typeof useCircuitSimulation>>();
+provideCircuitSimulation(
+  currentSimulation as ShallowRef<ReturnType<typeof useCircuitSimulation>>,
+);
+
+const currentTab = ref<string>();
+const tabItems = computed<TabBarItem[]>(() => {
+  const items: TabBarItem[] = simulations.map((sim) => ({
+    name: String(sim.id),
+    label: sim.circuitFactory.value?.label ?? `Untitled (${sim.id})`,
+    closeable: true,
+  }));
+  return items;
+});
+
+watch(currentSimulation, (sim) => {
+  currentTab.value = sim ? String(sim.id) : undefined;
+});
+
+function openNewSimulation(open: boolean = true) {
+  const sim = useCircuitSimulation();
+  simulations.push(sim);
+  if (open) {
+    currentSimulation.value = sim;
+  }
+  return sim;
+}
+
+const { getLoader } = useCircuitLoaders();
+function loadLevel(levelName: string) {
+  const sim = openNewSimulation();
+  const loader = getLoader(levelName);
+  if (!loader) {
+    throw new Error(`Unknown loader: ${levelName}`);
+  }
+  sim.field.loadBlank(loader.width, loader.height, loader.pinRows);
+  sim.load(loader);
+}
+
+function onSelectedTab(name: string) {
+  const sim = simulations.find((sim) => String(sim.id) === name);
+  if (sim) {
+    currentSimulation.value = sim;
+  }
+}
+
+function getSimulationByName(name: string) {
+  return simulations.find((sim) => String(sim.id) === name);
+}
+
+function onCloseTab(name: string) {
+  const index = simulations.findIndex((sim) => String(sim.id) === name);
+  if (index >= 0) {
+    const sim = simulations[index];
+    simulations.splice(index, 1);
+    if (currentSimulation.value === sim) {
+      const nextSim = simulations[index] ?? simulations[index - 1] ?? undefined;
+      currentSimulation.value = nextSim;
+    }
+  }
+}
+
+useMenuBarListener((event) => {
+  const { id } = event;
+  if (!id) return;
+  if (id.startsWith('level/')) {
+    const loaderKey = id.slice('level/'.length);
+    loadLevel(loaderKey);
+  }
+});
+
+useWelcomeDialogListener((event) => {
+  switch (event.action) {
+    case 'start-tutorial':
+      loadLevel('Tutorial 01 Introduction');
+      break;
+  }
+});
 </script>
 
 <style scoped>
