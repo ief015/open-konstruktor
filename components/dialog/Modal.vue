eb6381be25e6e4b271620b5e133c22031370b007
Nate <ief015@gmail.com>
2026-07-24 14:10:02 -0400

    Modal: click-outside emit, blur backdrop

--- components/dialog/Modal.vue
+++ components/dialog/Modal.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="fixed inset-0 z-40 bg-black bg-opacity-80">
-    <div class="fixed inset-0 flex items-center justify-center">
-      <div class="bg-neutral-600 p-4 rounded max-w-[80vw] max-h-[80vh] overflow-auto">
+  <div
+    class="fixed inset-0 z-40 bg-black bg-opacity-25 backdrop-blur-sm"
+    style="backdrop-filter: blur(4px)"
+  >
+    <div
+      class="fixed inset-0 flex items-center justify-center"
+      @click="emit('click-outside')"
+    >
+      <div
+        class="bg-neutral-600 p-4 rounded max-w-[80vw] max-h-[80vh] overflow-auto"
+        @click.stop
+      >
         <slot />
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  'click-outside': [];
+}>();
+</script>
